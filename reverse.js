23b2df85095662e9e1cc1d68d7fe7444ea7a1f97
Oleg Komarov <kamarowwww@gmail.com>
2018-02-28 18:40:32 +0100

    reverse

---
+++ reverse.js
@@ -0,0 +1,8 @@
+function Slovo() 
+{
+    var word1=document.reverse.word1.value;
+  //  var word = prompt('Введите слово', '');
+    console.log(word = word1.split('').reverse().join(''));
+    alert(word);
+
+}
