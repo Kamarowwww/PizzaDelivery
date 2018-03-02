a2de98348dfaa0b746a187b672a8952b622c09f5
Oleg Komarov <kamarowwww@gmail.com>
2018-03-02 15:11:07 +0100

    goodwork math

--- math.js
+++ math.js
@@ -1,8 +1,7 @@
-var a;
-var b;
+var firstnumber;
+var secondnumber;
 var symbol;
-var answer;
-var youranswer;
+
 function randomInteger(min, max) {
 
     var rand = min - 0.5 + Math.random() * (max - min + 1)
@@ -12,49 +11,77 @@ function randomInteger(min, max) {
 
 
 function generate() {
-    a = document.math.number1.value;
-    b = document.math.number2.value;
+   var a = document.math.number1.value;
+   var b = document.math.number2.value;
     firstnumber = randomInteger(a, b);
-    secondnumber = randomInteger(a, b);
+	secondnumber = randomInteger(a, b);
+	while (!isRandomNumberValid(secondnumber)) {
+	console.log("secondnumber", secondnumber);
+    		secondnumber = randomInteger(a, b);
+	}
     symbol = document.math.symbol.value;
-
     document.getElementById("result1").value = firstnumber + symbol + secondnumber;
-
+    document.getElementById("youranswer").disabled = false;
+    document.getElementById("number1").disabled = true;
+	document.getElementById("number2").disabled = true;
 }
-function result() {
+
+function checkResult(){
+	var correctAnswer = getCorrectAnswer();
+	var userAnswer =document.math.youranswer.value;
+	checkIfMatches(correctAnswer, userAnswer);
+	document.getElementById("result2").value = correctAnswer;
+}
+
+function getCorrectAnswer() {
+	var correctAnswer = null;
     switch (symbol) {
         case "+":
             {
-                answer = firstnumber + secondnumber;
+                correctAnswer = firstnumber + secondnumber;
                 break;
             }
         case "-":
             {
-                answer = firstnumber - secondnumber;
+                correctAnswer = firstnumber - secondnumber;
                 break;
             }
         case "/":
             {
-                answer = firstnumber / secondnumber;
+                correctAnswer = firstnumber / secondnumber;
                 break;
             }
         case "*":
             {
-                answer = firstnumber * secondnumber;
+                correctAnswer = firstnumber * secondnumber;
                 break;
             }
-
     }
-    document.getElementById("result2").value = answer;
-
+	return correctAnswer;
 
 }
-function match()
+function isRandomNumberValid(number)
+{
+	if(symbol == "/" && number==0)
+	{
+		return false;
+	}
+	return true;
+}
+
+function onAnswerChange() {
+	if (document.getElementById("youranswer").value !== "") {
+		document.getElementById("fBf").disabled = false;
+	}
+}
+
+function checkIfMatches(firstNumber, secondNumber)
 { 
-     youranswer=document.math.youranswer.value;
-    if (youranswer==answer)
+     
+    if (firstNumber==secondNumber)
     {
         alert('true');
+		
         localStorage.clickcount = (localStorage.clickcount) ? Number(localStorage.clickcount) + 1: 1;
         target.innerHTML = localStorage.clickcount || 0;
     }
@@ -66,24 +93,3 @@ function match()
         target1.innerHTML = localStorage.clickcount || 0;
     }
 }
-/*function countClicks() 
-{            
-    localStorage.clickcount = (localStorage.clickcount) ? Number(localStorage.clickcount) + 1: 1; update();
-}   
-function update() 
-{      
-    target.innerHTML = localStorage.clickcount || 0;
-}
-   */
- 
- 
-   /* function countClicks()
-  {
-    localStorage.clickcount = (localStorage.clickcount) ? Number(localStorage.clickcount) + 1: 1;
-    target.innerHTML = localStorage.clickcount || 0;
-  }  
-  function countClicks1()
-  {
-    localStorage.clickcount = (localStorage.clickcount) ? Number(localStorage.clickcount) + 1: 1;
-    target1.innerHTML = localStorage.clickcount || 0;
-  }  */
